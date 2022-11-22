cd790bd9366de719a1a890525335b37c54b4b06d
FemDazai <gadyrovasabina@gmail.com>
2022-11-22 20:39:26 +0400

    Initial commit

---
+++ SwaggerDogSittersTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SwaggerDogSittersTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
