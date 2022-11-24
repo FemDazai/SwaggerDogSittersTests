e573817a3fd4795676114e673601db777bd09a35
El-ItsMe <elnazteymurova001@gmail.com>
2022-11-25 01:55:23 +0400

    delete or change next time

--- SwaggerDogSittersTests/Client/NewClientsClient.cs
+++ SwaggerDogSittersTests/Client/NewClientsClient.cs
@@ -53,10 +53,33 @@ namespace SwaggerDogSittersTests.Client
 
             HttpStatusCode actualCode = responseMessage.StatusCode;
 
-
             string token = responseMessage.Content.ReadAsStringAsync().Result;
 
             return token;
+        }
+        public int RegistrationSitters(SittersResponseModel model)
+        {
+            HttpStatusCode expectedCode = HttpStatusCode.Created;
+            string jsone = JsonSerializer.Serialize<SittersResponseModel>(model);
+
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            HttpClient client = new HttpClient(clientHandler);
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new System.Uri($"https://piter-education.ru:10000/Sitters"),
+                Content = new StringContent(jsone, Encoding.UTF8, "application/json")
+            };
+            HttpResponseMessage responseMessage = client.Send(message);
+
+            HttpStatusCode actualCode = responseMessage.StatusCode;
+            Assert.AreEqual(expectedCode, actualCode);
+
+            int id = Convert.ToInt32(responseMessage.Content.ReadAsStringAsync().Result);
+
+            return id;
         }
     }
 }
--- SwaggerDogSittersTests/RegistrationTests.cs
+++ SwaggerDogSittersTests/RegistrationTests.cs
@@ -10,6 +10,7 @@ namespace SwaggerDogSittersTests
         [Test]
         public void RegistrationAndAuthClientTest()
         {
+            //У нас есть класс, который ведёт общение с сервером. Создаём объект это класса
             NewClientsClient client = new NewClientsClient();
             ClientsResponseModel clientsRequestModel = new ClientsResponseModel()
             {
@@ -21,7 +22,7 @@ namespace SwaggerDogSittersTests
                 Address = "1234567890",
                 Promocode = "string"
             };
-            
+
             int id = client.RegistrationClient(clientsRequestModel);
 
             AuthRequestModel authRequestModel = new AuthRequestModel()
@@ -32,6 +33,32 @@ namespace SwaggerDogSittersTests
             string actualToken = client.Auth(authRequestModel);
 
             Assert.NotNull(actualToken);
+        }
+        public void RegistrationAndAuthSittersTest()
+        {
+            NewClientsClient client = new NewClientsClient();
+            SittersResponseModel sittersrequestModel = new SittersResponseModel()
+            {
+                Name = "Zinaida",
+                LastName = "Pavlova",
+                Phone = "phone",
+                Email = "zinaidapavlovna@mail.ru",
+                Password = "123456789",
+                Age = 130,
+                Experience = 23,
+                Sex = 0,
+                Description = "string",
+                PriceCatalog = new List<PriceCatalogRequest>
+                {
+                    new PriceCatalogRequest
+                    {
+                        Service = 1,
+                        Price = 5000
+                    }
+                }
+            };
+            int id = client.RegistrationSitters(sittersrequestModel);
+            //У нас есть класс, который ведёт общение с сервером. Создаём объект это класса
         }
     }
 }
