7db9cd88d30cb961e7efdb9c3499997005ce0d65
El-ItsMe <elnazteymurova001@gmail.com>
2022-11-29 20:54:01 +0400

    added search class/model

--- SwaggerDogSittersTests/Client/NewClientsClient.cs
+++ SwaggerDogSittersTests/Client/NewClientsClient.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Text.Json;
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 
 
 namespace SwaggerDogSittersTests.Client
@@ -114,6 +115,38 @@ namespace SwaggerDogSittersTests.Client
             Assert.AreEqual(expectedCode, actualCode);
             string responseJsone = responseMessage.Content.ReadAsStringAsync().Result;
             List<GetSittersResponseModel> sitters = JsonSerializer.Deserialize<List<GetSittersResponseModel>>(responseJsone)!;
+            return sitters;
+        }
+
+        public List<SitterResponseModel> GetSearch(SearchRequestModel model)
+        { 
+            HttpStatusCode expectedCode = HttpStatusCode.OK;
+            string json = JsonSerializer.Serialize<SearchRequestModel>(model);
+
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            HttpClient client = new HttpClient(clientHandler);
+
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new System.Uri($"https://piter-education.ru:10000/Sitters"),
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+            HttpResponseMessage responseMessage = client.Send(message);
+            HttpStatusCode actualCode = responseMessage.StatusCode;
+
+            Assert.AreEqual(expectedCode, actualCode);
+            // Iz-za specifici protokola hhtp, soobwenie zakodirovano. Nujno rasskodirovat
+            string responseJson = responseMessage.Content.ReadAsStringAsync().Result;
+            //1.ResponseMessage - blok samoqo soobweniya (obrawaemsa k pismu);
+            //2.Content = eqo soderjanie (obrawaemsa k contentu);
+            //3.ReadAsStringAsync - iz-za napravleniya mnogo-potocnogo progrramirovaniya, nam mojet ponadobitsa, neskolko vivodov reziltata, a tut mi utocnsyem, kakoe imenno nam nujno 
+            //(Ctobi paskodirovat kontent, obrawaet eqo v stringu)
+            //async-asinxronniy
+
+            List<SitterResponseModel> sitters = JsonSerializer.Deserialize<List<SitterResponseModel>>(responseJson)!;
             return sitters;
         }
     }
