649cfcf24616695c85fa89df31d65ca4a1bb3a9d
El-ItsMe <elnazteymurova001@gmail.com>
2022-12-03 15:47:15 +0400

    Added class GetInfoByIdTest and some methods

---
+++ SwaggerDogSittersTests/GetInfoByIdTest.cs
@@ -0,0 +1,47 @@
+﻿using SwaggerDogSittersTests.Client;
+using SwaggerDogSittersTests.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwaggerDogSittersTests
+{
+    public class GetInfoByIdTest
+    {
+        [Test]
+        public void GetSitterInfoByIdTest()
+        {
+            //� ��� ���� �����, ������� ���� ������� � ��������. ������ ������ ��� ������
+            SittersClient sitterClient = new SittersClient();
+            SittersRequestModel sittersrequestModel = new SittersRequestModel()
+            {
+                Name = "Zinaida",
+                LastName = "Pavlova",
+                Phone = "+71234567890",
+                Email = "ZZ1Z12221iinaidapavlovnaa1@mail.ru",
+                Password = "123456789",
+                Age = 130,
+                Experience = 23,
+                Sex = 1,
+                Description = "string",
+                PriceCatalog = new List<PriceCatalogRequest>
+                    {
+                        new PriceCatalogRequest
+                        {
+                            Service = 1,
+                            Price = 5000
+                        }
+                    }
+            };
+
+            int actualid = sitterClient.RegistrationSitters(sittersrequestModel);
+            Assert.NotNull(actualid);
+
+            SitterIdResponseModel model = sitterClient.GetSitterInfoById(actualid);
+
+            Assert.NotNull(model);
+        }
+    }
+}
---
+++ SwaggerDogSittersTests/Models/SitterIdResponseModel.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SwaggerDogSittersTests.Models
+{
+    public class SitterIdResponseModel
+    {
+        public class PriceCatalogSitterId
+        {
+            [JsonPropertyName("service")]
+            public int service { get; set; }
+
+            [JsonPropertyName("price")]
+            public int price { get; set; }
+        }
+
+        public class GetSitterIdResponseModel
+        {
+            [JsonPropertyName("id")]
+            public int id { get; set; }
+
+            [JsonPropertyName("name")]
+            public string name { get; set; }
+
+            [JsonPropertyName("lastName")]
+            public string lastName { get; set; }
+
+            [JsonPropertyName("phone")]
+            public string phone { get; set; }
+
+            [JsonPropertyName("email")]
+            public string email { get; set; }
+
+            [JsonPropertyName("age")]
+            public int age { get; set; }
+
+            [JsonPropertyName("experience")]
+            public int experience { get; set; }
+
+            [JsonPropertyName("sex")]
+            public int sex { get; set; }
+
+            [JsonPropertyName("description")]
+            public string description { get; set; }
+
+            [JsonPropertyName("priceCatalog")]
+            public List<PriceCatalogSitterId> priceCatalog { get; set; }
+
+            [JsonPropertyName("isDeleted")]
+            public bool isDeleted { get; set; }
+
+            [JsonPropertyName("regestrationDate")]
+            public string regestrationDate { get; set; }
+        }
+
+    }
+}
