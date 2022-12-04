d324259c1ece36ee5cfd85e5dc2166621f94f96f
FemDazai <gadyrovasabina@gmail.com>
2022-12-04 19:23:09 +0400

    Add new test

---
+++ SwaggerDogSittersTests/Models/ClientIdResponseModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SwaggerDogSittersTests.Models
+{
+    public class ClientIdResponseModel
+    {
+        [JsonPropertyName("id")]
+        public int id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+
+        [JsonPropertyName("lastName")]
+        public string lastName { get; set; }
+
+        [JsonPropertyName("phone")]
+        public string phone { get; set; }
+
+        [JsonPropertyName("email")]
+        public string email { get; set; }
+
+        [JsonPropertyName("address")]
+        public string address { get; set; }
+
+        [JsonPropertyName("registrationDate")]
+        public DateTime registrationDate { get; set; }
+
+        [JsonPropertyName("dogs")]
+        public List<object> dogs { get; set; }
+
+        [JsonPropertyName("orders")]
+        public List<object> orders { get; set; }
+
+        [JsonPropertyName("isDeleted")]
+        public bool isDeleted { get; set; }
+    }
+}
