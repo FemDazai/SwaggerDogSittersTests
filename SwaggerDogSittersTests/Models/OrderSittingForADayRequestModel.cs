b25b4028231ea2d201418bc0b5fd8f166aa36160
El-ItsMe <elnazteymurova001@gmail.com>
2022-12-05 03:01:19 +0400

    Added new orderservice model X method

---
+++ SwaggerDogSittersTests/Models/OrderSittingForADayRequestModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SwaggerDogSittersTests.Models
+{
+    public class OrderSittingForADayRequestModel
+    {
+        [JsonPropertyName("clientId")]
+        public int clientId { get; set; }
+
+        [JsonPropertyName("sitterId")]
+        public int sitterId { get; set; }
+
+        [JsonPropertyName("animalIds")]
+        public List<int> animalIds { get; set; }
+
+        [JsonPropertyName("status")]
+        public int status { get; set; }
+
+        [JsonPropertyName("workDate")]
+        public string workDate { get; set; }
+
+        [JsonPropertyName("district")]
+        public int district { get; set; }
+
+        [JsonPropertyName("visitQuantity")]
+        public int visitQuantity { get; set; }
+    }
+}
